78fc091d68458359477f8318dafa9b96262798aa
Soumay kanti Das <anikds2859@outlook.com>
2021-05-30 19:00:35 +0600

    Add project files.

---
+++ Elseif-another/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Elseif_another
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a chracter: ");
+            char c = (char)Console.Read();
+            if(char.IsUpper(c))
+            {
+                Console.WriteLine("character is upper case");
+
+
+            }
+            else if(char.IsLower(c))
+            {
+                Console.WriteLine("Charecter is in lower case");
+            }
+            else if(char.IsDigit(c))
+            {
+                Console.WriteLine("This is a Digit");
+            }
+            else
+            {
+                Console.WriteLine("This is not alphabetic");
+            }
+        }
+    }
+}
